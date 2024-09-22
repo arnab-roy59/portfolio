6ef4cb26680287cbb7edae8c60f4877cf5cbf4da
Arnab Roy <arnabr0509@gmail.com>
2024-09-22 17:08:59 +0530

    from validation

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+        //Navbar
 $(document).ready(function(){
     $(window).scroll(function(){
         if(this.scrollY>20){
@@ -25,6 +26,35 @@ $(document).ready(function(){
         backSpeed: 60,
         loop: true
     });
+});
+
+        //Form Validation
+document.getElementById("contactForm").addEventListener("submit", function(event){
+    event.preventDefault();
+
+    const name = document.getElementById('name').value.trim();
+    const email = document.getElementById("email").value.trim();
+    const subject = document.getElementById("subject").value.trim();
+    const message = document.getElementById("message").value.trim();
+
+    if (name === "" || !/^[a-zA-Z ]+$/.test(name)) {
+        alert("Please enter a valid name (letters only).");
+        return;
+    }
+    let emailPattern = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if (!emailPattern.test(email)) {
+        alert("Please enter a valid email address.");
+        return;
+    }
+    if (subject === "") {
+        alert("Subject cannot be empty.");
+        return;
+    }
+    if (message === "") {
+        alert("Message cannot be empty.");
+        return;
+    }
+    alert("Form submitted successfully!");
 });
 
 
